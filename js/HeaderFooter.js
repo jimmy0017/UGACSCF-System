261db1f12414eead7ed765f04419e20ee76ff4ec
Jimmy Lynn <jimmy0017@gmail.com>
2015-11-03 20:37:40 -0500

    Nov-3-2015
    Nov-3-2015 20:37

---
+++ js/HeaderFooter.js
@@ -0,0 +1,9 @@
+$(function() {
+	$("#header").load("header.html");
+	$("#footer").load("footer.html");
+		
+});
+/*
+$(document).ready(function() {
+	$('a[href="' + this.location.pathname + '"]').parent().addClass('active');
+});*/
